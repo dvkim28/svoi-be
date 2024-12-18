c02cde107779246230f42990a81f262f744efef3
dmitriy kim <d.villarionovich@gmail.com>
2024-12-18 23:34:50 +0100

    API for retrieve category/sub-category/ad

--- advr/urls.py
+++ advr/urls.py
@@ -1,20 +1,19 @@
-from django.urls import path
-from advr.views import SubCategoryViewSet, AdViewSet, CategoryViewSet
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from advr.views import CategoryViewSet, SubCategoryViewSet, AdViewSet
+
+router = DefaultRouter()
+
+router.register(r"categories", CategoryViewSet, basename="category")
+router.register(r"categories/(?P<category_slug>"
+                r"[^/]+)/subcategories",
+                SubCategoryViewSet,
+                basename="subcategory")
+router.register(r"categories/(?P<category_slug>[^/]+)"
+                r"/subcategories/(?P<sub_category_slug>[^/]+)/ads",
+                AdViewSet,
+                basename="ad")
 
 urlpatterns = [
-    path(
-        "<slug:slug>/",
-        CategoryViewSet.as_view({'get': 'retrieve'}),
-        name="category-detail",
-    ),
-    path(
-        "<slug:category_slug>/<slug:sub_category_slug>/",
-        SubCategoryViewSet.as_view({'get': 'retrieve'}),
-        name="subcategory-detail",
-    ),
-    path(
-        "<slug:category_slug>/<slug:sub_category_slug>/<slug:ad_slug>/",
-        AdViewSet.as_view({'get': 'retrieve'}),
-        name="ad-detail",
-    ),
+    path('', include(router.urls)),
 ]
--- advr/views.py
+++ advr/views.py
@@ -1,13 +1,13 @@
 from rest_framework import mixins
-from rest_framework.generics import get_object_or_404
+from rest_framework.exceptions import NotFound
 from rest_framework.viewsets import GenericViewSet
-
 from advr.models import Category, SubCategory, Ad
 from advr.serializers import (
     CategorySerializer,
     SubCategorySerializer,
     AdSerializer
 )
+from django.shortcuts import get_object_or_404
 
 
 class CategoryViewSet(
@@ -22,37 +22,31 @@ class CategoryViewSet(
 
 class SubCategoryViewSet(
     GenericViewSet,
+    mixins.ListModelMixin,
     mixins.RetrieveModelMixin,
 ):
     queryset = SubCategory.objects.all()
     serializer_class = SubCategorySerializer
+    lookup_field = "slug"
 
-    def get_object(self):
-        queryset = self.get_queryset()
+    def get_queryset(self):
         category_slug = self.kwargs.get("category_slug")
-        sub_category_slug = self.kwargs.get("sub_category_slug")
-        return get_object_or_404(
-            queryset,
-            category__slug=category_slug,
-            slug=sub_category_slug
-        )
+        return SubCategory.objects.filter(category__slug=category_slug)
 
 
 class AdViewSet(
     GenericViewSet,
+    mixins.ListModelMixin,
     mixins.RetrieveModelMixin,
 ):
     queryset = Ad.objects.all()
     serializer_class = AdSerializer
+    lookup_field = "slug"
 
-    def get_object(self):
-        queryset = self.get_queryset()
+    def get_queryset(self):
         category_slug = self.kwargs.get("category_slug")
         sub_category_slug = self.kwargs.get("sub_category_slug")
-        ad_slug = self.kwargs.get("ad_slug")
-        return get_object_or_404(
-            queryset,
+        return Ad.objects.filter(
             sub_category__slug=sub_category_slug,
-            sub_category__category__slug=category_slug,
-            slug=ad_slug
+            sub_category__category__slug=category_slug
         )
